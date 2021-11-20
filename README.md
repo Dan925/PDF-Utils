6fcbce8b69d577d67e0ae5614edd2f542a91ca99
Dan <dngen029@uottawa.ca>
2021-11-20 12:29:55 -0500

    add new function to replace a page and change merge_pdf_in_dir to order by filename

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Python script to manipulate pdf files
 
 # Operations
 
-1. Merge all pdf files in a directory.
+1. Combine all pdf files in a folder into one (ordered by file name).
 2. Move a page in pdf file to a specific page number
-3. Merge a pdf file into another at after a specific page
+3. Replace a page in a pdf file with pages from another file
+4. Merge a pdf file into another at after a specific page
 
 # Setup
 
--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import os
 from PyPDF2  import PdfFileMerger,PdfFileWriter, PdfFileReader
 
 
+
+
+def replace_page():
+    base = str(input("Provide a path to the PDF file to use as base: "))
+    input1 = str(input("Provide a path to the PDF file to use as input: "))
+    if not (os.path.isfile(base) and os.path.isfile(input1) and os.path.splitext(base)[1] == ".pdf" and os.path.splitext(input1)[1] == ".pdf"):
+        print("these paths don't lead to existing pdf files")
+        return
+    page = int(input("Provide a page number in the base PDF file where to replace by the new pdf (page number ?): "))
+    baseReader = PdfFileReader(base)
+    inputReader = PdfFileReader(input1)
+    if page > baseReader.getNumPages() or page < 1 :
+        print("invalid page number")
+        return
+    page-=1
+    baseWriter = PdfFileWriter()
+    for i in range(baseReader.getNumPages()):
+        if i != page:
+            baseWriter.addPage(baseReader.getPage(i))
+        else:
+            baseWriter.appendPagesFromReader(inputReader)
+    baseWriter.write(open(base,"wb"))
+    print("Done, your base pdf file has been modified")
+
+
 def move_pdf_page():
     base = str(input("Provide a path to the PDF file: "))
     if not (os.path.isfile(base) and os.path.splitext(base)[1] == ".pdf"):
@@ -22,8 +47,8 @@ def move_pdf_page():
             baseWriter.addPage(baseReader.getPage(i))
     pageObj = baseReader.getPage(page)
     baseWriter.insertPage(pageObj,position)
-
     baseWriter.write(open(base,"wb"))
+    print("Done, your base pdf file has been modified")
 
 
 
@@ -48,40 +73,47 @@ def merge_pdf_after_page():
         output = open(out_path,"wb")
         merger.write(output)
         merger.close()
+        print(f'Done, your new pdf file should be at: {out_path}')
     except:
         print("Exception error occured")
 
 def merge_pdf_in_dir():
-    path = str(input("Provide a directory path where you want to merge all pdf files: "))
+    path = str(input("Provide a folder path where you want to merge all pdf files: "))
     out_name = str(input("Provide a file name for the output pdf file : "))+".pdf"
     if os.path.isdir(path):
         merger = PdfFileMerger()
-        for filename in os.listdir(path):
+        for filename in sorted(filter(lambda x: os.path.isfile(os.path.join(path,x)),os.listdir(path))):
             f = os.path.join(path,filename)
-            if os.path.isfile(f) and os.path.splitext(f)[1] == ".pdf":
-                # print(filename)
+            if os.path.splitext(f)[1] == ".pdf":
+                print(filename)
                 pdfFile = open(f,"rb")
-
                 merger.append(pdfFile)
 
         out_path = os.path.join(path,out_name)
         output = open(out_path,"wb")
         merger.write(output)
         merger.close()
+        print(f'Done, your new pdf file should be at: {out_path}')
     else:
         print("This path isn't a directory")
 
 
 def main():
-    choice = int(input ("Enter 1 for merge all pdf in directory.\nEnter 2 to move a page in PDF file to position x.\nEnter 3 for merge two pdfs after page x.\n"))
+    choice = int(input ("Enter 1 for merge all pdf in directory(ordered by filename).\nEnter 2 to move a page in PDF file to position x.\nEnter 3 to replace a page with a new pdf file.\nEnter 4 for merge two pdfs after page x.\n"))
     if choice == 1:
         merge_pdf_in_dir()
     elif choice == 2:
         move_pdf_page()
     elif choice == 3:
+        replace_page()
+    elif choice == 4:
         merge_pdf_after_page()
     else:
         print("Invalid choice")
 
 if __name__=="__main__":
     main()
+
+
+# /Users/dan/Desktop/SEG3503/Devoir-3-Questions.pdf
+# /Users/dan/Desktop/SEG3503/merge-out.pdf
